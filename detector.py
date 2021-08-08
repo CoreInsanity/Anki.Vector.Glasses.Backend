96c50a33fe5f6a6deab53088a45b6e2b7c0b66aa
maxim van den eede <maximvde@gmail.com>
2021-08-08 11:29:55 +0200

    Cleanup & improvements

--- detector.py
+++ detector.py
@@ -1,19 +1,22 @@
 from imageai.Detection import ObjectDetection
 import os
 
-def detect(inputFile, minProbability):
-    model = os.path.join(os.getcwd(), "models\\yolo.h5")
-    #model = os.path.join(os.getcwd(), "models\\resnet50_coco_best_v2.1.0.h5")
-
+class Detection():
     detector = ObjectDetection()
-    #detector.setModelTypeAsRetinaNet()
-    detector.setModelTypeAsYOLOv3()
-    #detector.setModelTypeAsTinyYOLOv3()
-    detector.setModelPath(model)
-    detector.loadModel(detection_speed="normal")
-    detections = detector.detectObjectsFromImage(input_image=inputFile, output_type="array", minimum_percentage_probability=minProbability)
 
-    detectionDict = []
-    for eachObject in detections[1]:
-        detectionDict.append((eachObject["name"], eachObject["percentage_probability"]))
-    return detectionDict
+    def __init__(self):
+        model = os.path.join(os.getcwd(), "models\\yolo.h5")
+        #model = os.path.join(os.getcwd(), "models\\resnet50_coco_best_v2.1.0.h5")
+        #detector.setModelTypeAsRetinaNet()
+        self.detector.setModelTypeAsYOLOv3()
+        #detector.setModelTypeAsTinyYOLOv3()
+        self.detector.setModelPath(model)
+        self.detector.loadModel(detection_speed="normal")
+
+    def detect(self, inputFile, minProbability):
+        detections = self.detector.detectObjectsFromImage(input_image=inputFile, output_type="array", minimum_percentage_probability=minProbability)
+
+        detectionDict = []
+        for eachObject in detections[1]:
+            detectionDict.append((eachObject["name"], eachObject["percentage_probability"]))
+        return detectionDict
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from detector import detect
+from detector import Detection
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import os
@@ -10,6 +10,8 @@ serverPort = 8080
 minProbability = 50
 
 class FileUploadServer(BaseHTTPRequestHandler):
+    detection = Detection()
+    
     def do_GET(self):
         print("Received GET request")
         filename = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
@@ -22,8 +24,8 @@ class FileUploadServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes("{", "utf-8"))
         print("Valid image found, detecting objects...")
-        for detection in detect(filename, minProbability):
-            self.wfile.write(bytes("""\"{0}\": {1},""".format(str(detection[0]), str(detection[1])), "utf-8"))
+        for detected in self.detection.detect(filename, minProbability):
+            self.wfile.write(bytes("""\"{0}\": {1},""".format(str(detected[0]), str(detected[1])), "utf-8"))
         self.wfile.write(bytes("}", "utf-8"))
         os.remove(filename)
         print("Object detection completed")
@@ -31,7 +33,6 @@ class FileUploadServer(BaseHTTPRequestHandler):
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), FileUploadServer)
-
     print("Awaiting commands...")
 
     try:
